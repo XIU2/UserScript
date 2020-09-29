ffd04ca99e6c40141b9855cc60db615cc5b56c72
xiu2 <54703944+XIU2@users.noreply.github.com>
2020-09-29 22:34:47 +0800

    update

--- SteamWorkshopImageRepair.user.js
+++ SteamWorkshopImageRepair.user.js
@@ -8,7 +8,7 @@
 // @description:zh-TW    修復 Steam 創意工坊預覽大圖無法顯示的問題。
 // @include              *://steamcommunity.com/sharedfiles/filedetails/*
 // @include              *://steamcommunity.com/workshop/filedetails/*
-// @version              1.0
+// @version              1.0.0
 // @icon                 https://store.steampowered.com/favicon.ico
 // @run-at               document-end
 // @namespace            https://greasyfork.org/scripts/397666
