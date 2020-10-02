063626d297009197585ca6b443bbd78810ba15c4
xiu2 <54703944+XIU2@users.noreply.github.com>
2020-10-02 14:10:37 +0800

    规范 match

--- GithubEnhanced-High-Speed-Download.user.js
+++ GithubEnhanced-High-Speed-Download.user.js
@@ -3,7 +3,9 @@
 // @version      1.0.3
 // @author       X.I.U
 // @description  为 Github 的 Releases、Code(ZIP) 添加高速下载
-// @match        *://github.com/*
+// @match        https://github.com/*/*
+// @match        https://github.com/*/*/releases
+// @match        https://github.com/*/*/releases/*
 // @icon         https://github.githubassets.com/favicon.ico
 // @require      https://cdn.bootcss.com/jquery/3.4.1/jquery.min.js
 // @license      GPL-3.0 License
@@ -13,15 +15,15 @@
 
 (function() {
     var source_code = false; // Source code 加速，false=关闭，true=开启
-    var download_url1 = "https://download.fastgit.org"; // 日本东京服务器
+    var download_url1 = "https://download.fastgit.org";
     var download_url1_name = "日本东京";
-    var download_url2 = "https://github.wuyanzheshui.workers.dev"; // Cloudflare CDN 美国服务器
+    var download_url2 = "https://github.wuyanzheshui.workers.dev";
     var download_url2_name = "美国 01";
-    var download_url3 = "https://gh.api.99988866.xyz"; // Cloudflare CDN 美国服务器
+    var download_url3 = "https://gh.api.99988866.xyz";
     var download_url3_name = "美国 02";
-    var download_url4 = "https://g.ioiox.com"; // 腾讯 中国香港服务器
+    var download_url4 = "https://g.ioiox.com";
     var download_url4_name = "中国香港";
-    var download_url5 = "https://git.yumenaka.net"; // 搬瓦工 美国洛杉矶服务器
+    var download_url5 = "https://git.yumenaka.net";
     var download_url5_name = "美国洛杉矶";
     var download_zip_svg = `<svg class="octicon octicon-file-zip mr-3" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M3.5 1.75a.25.25 0 01.25-.25h3a.75.75 0 000 1.5h.5a.75.75 0 000-1.5h2.086a.25.25 0 01.177.073l2.914 2.914a.25.25 0 01.073.177v8.586a.25.25 0 01-.25.25h-.5a.75.75 0 000 1.5h.5A1.75 1.75 0 0014 13.25V4.664c0-.464-.184-.909-.513-1.237L10.573.513A1.75 1.75 0 009.336 0H3.75A1.75 1.75 0 002 1.75v11.5c0 .649.353 1.214.874 1.515a.75.75 0 10.752-1.298.25.25 0 01-.126-.217V1.75zM8.75 3a.75.75 0 000 1.5h.5a.75.75 0 000-1.5h-.5zM6 5.25a.75.75 0 01.75-.75h.5a.75.75 0 010 1.5h-.5A.75.75 0 016 5.25zm2 1.5A.75.75 0 018.75 6h.5a.75.75 0 010 1.5h-.5A.75.75 0 018 6.75zm-1.25.75a.75.75 0 000 1.5h.5a.75.75 0 000-1.5h-.5zM8 9.75A.75.75 0 018.75 9h.5a.75.75 0 010 1.5h-.5A.75.75 0 018 9.75zm-.75.75a1.75 1.75 0 00-1.75 1.75v3c0 .414.336.75.75.75h2.5a.75.75 0 00.75-.75v-3a1.75 1.75 0 00-1.75-1.75h-.5zM7 12.25a.25.25 0 01.25-.25h.5a.25.25 0 01.25.25v2.25H7v-2.25z"></path></svg>`;
     var download_releases_style = `padding:0 4px;margin-right: -1px;border-radius: 2px;background-color: #ffffff;border-color: rgba(27, 31, 35, 0.1);font-size: 12px;`
