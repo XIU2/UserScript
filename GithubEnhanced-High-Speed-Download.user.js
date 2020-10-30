1f36f98a66c03b15cec5f0ade9b310f58db271f0
xiu2 <54703944+XIU2@users.noreply.github.com>
2020-10-30 12:37:41 +0800

    新增 加速源 [中国香港]；优化 按钮样式及文字

--- GithubEnhanced-High-Speed-Download.user.js
+++ GithubEnhanced-High-Speed-Download.user.js
@@ -105,11 +105,11 @@
         $(".Box.Box--condensed").each(function () {
             $(this).find(".d-flex.Box-body>a").each(function () {
                 var href = $(this).attr("href");
-                var url1 = download_url1 + '/github.com' + href;
-                var url2 = download_url2 + '/github.com' + href;
+                var url1 = download_url1 + '/https://github.com' + href;
+                var url2 = download_url2 + '/https://github.com' + href;
                 var url3 = download_url3 + href;
-                var url4 = download_url4 + '/github.com' + href;
-                var url5 = download_url5 + '/github.com' + href;
+                var url4 = download_url4 + '/https://github.com' + href;
+                var url5 = download_url5 + '/https://github.com' + href;
                 var url6 = download_url6 + '/https://github.com' + href;
                 var html1 = `<div style="display: flex;justify-content: flex-end;">
 <div><a style="${download_release_style}" class="btn" href="${url1}" rel="nofollow">${download_url1_name}</a></div>
@@ -128,12 +128,12 @@
             // Source Code 加速
             $(this).find(".d-block.Box-body>a").each(function () {
                 var href = $(this).attr("href");
-                var url1 = download_url1 + '/github.com' + href;
-                var url2 = download_url2 + '/github.com' + href;
+                var url1 = download_url1 + '/https://github.com' + href;
+                var url2 = download_url2 + '/https://github.com' + href;
                 var url3 = download_url3 + href;
-                var url4 = download_url4 + '/github.com' + href;
-                var url5 = download_url5 + '/github.com' + href;
-                var url6 = download_url6 + 'https:///github.com' + href;
+                var url4 = download_url4 + '/https://github.com' + href;
+                var url5 = download_url5 + '/https://github.com' + href;
+                var url6 = download_url6 + '/https://github.com' + href;
                 var html1 = `<div style="display: flex;justify-content: flex-end;flex-grow: 1;">
 <div><a style="${download_release_style}" class="btn" href="${url1}" rel="nofollow">${download_url1_name}</a></div>
 <div><a style="${download_release_style}" class="btn" href="${url2}" rel="nofollow">${download_url2_name}</a></div>
@@ -154,12 +154,12 @@
     function addDownloadZIP(){
         $(".dropdown-menu.dropdown-menu-sw.p-0 ul li:last-child").each(function () {
             var href = $(this).children("a").attr("href");
-            var url1 = download_url1 + "/github.com" + href;
-            var url2 = download_url2 + "/github.com" + href;
+            var url1 = download_url1 + "/https://github.com" + href;
+            var url2 = download_url2 + "/https://github.com" + href;
             var url3 = download_url3 + href;
-            var url4 = download_url4 + "/github.com" + href;
-            var url5 = download_url5 + "/github.com" + href;
-            var url6 = download_url6 + 'https:///github.com' + href;
+            var url4 = download_url4 + "/https://github.com" + href;
+            var url5 = download_url5 + "/https://github.com" + href;
+            var url6 = download_url6 + '/https://github.com' + href;
             var html1 = `
 <li class="Box-row Box-row--hover-gray p-0"><a class="d-flex flex-items-center text-gray-dark text-bold no-underline p-3" rel="nofollow" href="${url1}">${download_zip_svg}Download ZIP ${download_url1_name}</a></li>
 <li class="Box-row Box-row--hover-gray p-0"><a class="d-flex flex-items-center text-gray-dark text-bold no-underline p-3" rel="nofollow" href="${url2}">${download_zip_svg}Download ZIP ${download_url2_name}</a></li>
