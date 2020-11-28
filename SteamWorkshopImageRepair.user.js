c517dbbb8c7c4ea6252af55586eb749aceb8f4bd
xiu2 <54703944+XIU2@users.noreply.github.com>
2020-11-28 23:21:16 +0800

    update

--- SteamWorkshopImageRepair.user.js
+++ SteamWorkshopImageRepair.user.js
@@ -4,7 +4,7 @@
 // @version              1.0.0
 // @author               X.I.U
 // @description          Fixed Steam Workshop image not showing
-// @description:zh-CN    修复 Steam 创意工坊预览大图无法显示的问题。 
+// @description:zh-CN    修复 Steam 创意工坊预览大图无法显示的问题
 // @match                *://steamcommunity.com/sharedfiles/filedetails/*
 // @match                *://steamcommunity.com/workshop/filedetails/*
 // @icon                 https://store.steampowered.com/favicon.ico
