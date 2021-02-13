100d477e26874375adc1c490e65bc552d9a25d6c
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-02-13 12:38:25 +0800

    新增 [调整描述（话说）编辑框大小] 脚本菜单;优化 [调整描述（话说）编辑框大小]（调小了一点）

--- Lanzou-Enhanced.user.js
+++ Lanzou-Enhanced.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         蓝奏云网盘增强
-// @version      1.1.6
+// @version      1.1.7
 // @author       X.I.U
 // @description  刷新不返回根目录、右键文件显示菜单、自动显示更多文件、自动打开分享链接、自动复制分享链接、调整描述（话说）编辑框初始大小、拖入文件自动显示上传框
 // @match        *://*.lanzous.com/*
@@ -31,38 +31,44 @@
     var menu_open_fileSha = GM_getValue('xiu2_menu_open_fileSha'),
         menu_copy_fileSha = GM_getValue('xiu2_menu_copy_fileSha'),
         menu_refreshCorrection = GM_getValue('xiu2_menu_refreshCorrection'),
-        menu_rightClickMenu = GM_getValue('xiu2_menu_rightClickMenu');
-    var menu_open_fileSha_ID, menu_copy_fileSha_ID, menu_refreshCorrection_ID, menu_rightClickMenu_ID, menu_feedBack_ID;
+        menu_rightClickMenu = GM_getValue('xiu2_menu_rightClickMenu'),
+        menu_folderDescdesMenu = GM_getValue('xiu2_menu_folderDescdesMenu');
+    var menu_open_fileSha_ID, menu_copy_fileSha_ID, menu_refreshCorrection_ID, menu_rightClickMenu_ID, menu_folderDescdesMenu_ID, menu_feedBack_ID;
     if (menu_open_fileSha == null){menu_open_fileSha = true; GM_setValue('xiu2_menu_open_fileSha', menu_open_fileSha)};
     if (menu_copy_fileSha == null){menu_copy_fileSha = true; GM_setValue('xiu2_menu_copy_fileSha', menu_copy_fileSha)};
     if (menu_refreshCorrection == null){menu_refreshCorrection = true; GM_setValue('xiu2_menu_refreshCorrection', menu_refreshCorrection)};
     if (menu_rightClickMenu == null){menu_rightClickMenu = true; GM_setValue('xiu2_menu_rightClickMenu', menu_rightClickMenu)};
+    if (menu_folderDescdesMenu == null){menu_folderDescdesMenu = true; GM_setValue('xiu2_menu_folderDescdesMenu', menu_folderDescdesMenu)};
     registerMenuCommand();
 
     // 注册脚本菜单
     function registerMenuCommand() {
-        var menu_open_fileSha_, menu_copy_fileSha_, menu_refreshCorrection_, menu_rightClickMenu_;
+        var menu_open_fileSha_, menu_copy_fileSha_, menu_refreshCorrection_, menu_rightClickMenu_, menu_folderDescdesMenu_;
         if (menu_feedBack_ID){ // 如果反馈菜单ID不是 null，则删除所有脚本菜单
             GM_unregisterMenuCommand(menu_open_fileSha_ID);
             GM_unregisterMenuCommand(menu_copy_fileSha_ID);
             GM_unregisterMenuCommand(menu_refreshCorrection_ID);
             GM_unregisterMenuCommand(menu_rightClickMenu_ID);
+            GM_unregisterMenuCommand(menu_folderDescdesMenu_ID);
             GM_unregisterMenuCommand(menu_feedBack_ID);
             menu_open_fileSha = GM_getValue('xiu2_menu_open_fileSha');
             menu_copy_fileSha = GM_getValue('xiu2_menu_copy_fileSha');
             menu_refreshCorrection = GM_getValue('xiu2_menu_refreshCorrection');
             menu_rightClickMenu = GM_getValue('xiu2_menu_rightClickMenu');
+            menu_folderDescdesMenu = GM_getValue('xiu2_menu_folderDescdesMenu');
         }
 
         if (menu_open_fileSha){menu_open_fileSha_ = "√";}else{menu_open_fileSha_ = "×";}
         if (menu_copy_fileSha){menu_copy_fileSha_ = "√";}else{menu_copy_fileSha_ = "×";}
         if (menu_refreshCorrection){menu_refreshCorrection_ = "√";}else{menu_refreshCorrection_ = "×";}
         if (menu_rightClickMenu){menu_rightClickMenu_ = "√";}else{menu_rightClickMenu_ = "×";}
+        if (menu_folderDescdesMenu){menu_folderDescdesMenu_ = "√";}else{menu_folderDescdesMenu_ = "×";}
 
         menu_open_fileSha_ID = GM_registerMenuCommand(`[ ${menu_open_fileSha_} ] 自动打开分享链接`, function(){menu_switch(menu_open_fileSha,'xiu2_menu_open_fileSha','自动打开分享链接', true)});
         menu_copy_fileSha_ID = GM_registerMenuCommand(`[ ${menu_copy_fileSha_} ] 自动复制分享链接`, function(){menu_switch(menu_copy_fileSha,'xiu2_menu_copy_fileSha','自动复制分享链接', true)});
         menu_refreshCorrection_ID = GM_registerMenuCommand(`[ ${menu_refreshCorrection_} ] 刷新不返回根目录`, function(){if(menu_refreshCorrection){UNrefreshCorrection();}else{refreshCorrection();};menu_switch(menu_refreshCorrection,'xiu2_menu_refreshCorrection','刷新不返回根目录', false)});
         menu_rightClickMenu_ID = GM_registerMenuCommand(`[ ${menu_rightClickMenu_} ] 右键文件显示菜单`, function(){menu_switch(menu_rightClickMenu,'xiu2_menu_rightClickMenu','右键文件显示菜单', true)});
+        menu_folderDescdesMenu_ID = GM_registerMenuCommand(`[ ${menu_folderDescdesMenu_} ] 调整描述（话说）编辑框大小`, function(){menu_switch(menu_folderDescdesMenu,'xiu2_menu_folderDescdesMenu','调整描述（话说）编辑框大小', true)});
         menu_feedBack_ID = GM_registerMenuCommand('反馈 & 建议', function () {window.GM_openInTab('https://github.com/XIU2/UserScript#xiu2userscript', {active: true,insert: true,setParent: true});});
     }
 
@@ -200,17 +206,19 @@
 
     // 调整话说编辑框初始大小
     function folderDescdes() {
-        let folderdescdes = mainframe.document.getElementById("folder_descdes"); // 寻找话说（描述）编辑框
-        if(folderdescdes){ // 判断话说（描述）元素是否存在
-            folderdescdes.style.cssText="margin: 15px 0px; width: 666px; height: 150px;"
-        }
-        let folderdescdes2 = mainframe.document.getElementById("fol_credes"); // 寻找话说（描述）编辑框
-        if(folderdescdes2){ // 判断话说（描述）元素是否存在
-            folderdescdes2.style.cssText="margin: 15px 0px; width: 666px; height: 150px;"
-        }
-        let folderdescdes3 = mainframe.document.getElementById("file_desc"); // 寻找话说（描述）编辑框
-        if(folderdescdes3){ // 判断话说（描述）元素是否存在
-            folderdescdes3.style.cssText="margin: 15px 0px; width: 666px; height: 150px;"
+        if(menu_folderDescdesMenu) {
+            let folderdescdes = mainframe.document.getElementById("folder_descdes"); // 寻找话说（描述）编辑框
+            if(folderdescdes){ // 判断话说（描述）元素是否存在
+                folderdescdes.style.cssText="margin: 15px 0px; width: 550px; height: 125px;"
+            }
+            let folderdescdes2 = mainframe.document.getElementById("fol_credes"); // 寻找话说（描述）编辑框
+            if(folderdescdes2){ // 判断话说（描述）元素是否存在
+                folderdescdes2.style.cssText="margin: 15px 0px; width: 550px; height: 125px;"
+            }
+            let folderdescdes3 = mainframe.document.getElementById("file_desc"); // 寻找话说（描述）编辑框
+            if(folderdescdes3){ // 判断话说（描述）元素是否存在
+                folderdescdes3.style.cssText="margin: 15px 0px; width: 550px; height: 125px;"
+            }
         }
     }
 
