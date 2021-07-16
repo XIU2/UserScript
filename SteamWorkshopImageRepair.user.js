35fc4232ff8afaa0f7fbdadfa846cb037efd2cad
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-07-17 06:32:47 +0800

    Update

--- SteamWorkshopImageRepair.user.js
+++ SteamWorkshopImageRepair.user.js
@@ -1,16 +1,14 @@
 // ==UserScript==
-// @name                 Steam Workshop Image Repair
-// @name:zh-CN           Steam 创意工坊大图修复
-// @version              1.0.0
-// @author               X.I.U
-// @description          Fixed Steam Workshop image not showing
-// @description:zh-CN    修复 Steam 创意工坊预览大图无法显示的问题（Steam 不改的话，长期可用）
-// @match                *://steamcommunity.com/sharedfiles/filedetails/*
-// @match                *://steamcommunity.com/workshop/filedetails/*
-// @icon                 https://store.steampowered.com/favicon.ico
-// @license              GPL-3.0 License
-// @run-at               document-end
-// @namespace            https://greasyfork.org/scripts/397666
+// @name         Steam 创意工坊大图修复
+// @version      1.0.0
+// @author       X.I.U
+// @description  修复 Steam 创意工坊预览大图无法显示的问题（Steam 不改的话，长期可用）
+// @match        *://steamcommunity.com/sharedfiles/filedetails/*
+// @match        *://steamcommunity.com/workshop/filedetails/*
+// @icon         https://store.steampowered.com/favicon.ico
+// @license      GPL-3.0 License
+// @run-at       document-end
+// @namespace    https://github.com/XIU2/UserScript
 // ==/UserScript==
 
 if(typeof onYouTubeIframeAPIReady == 'function') {
