c09402dd9474c54b6e4ca51b913fc8728cb84e78
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-08-29 20:29:22 +0800

    新增 对 Discuz! 论坛各板块帖子列表的支持

--- TargetBlank.user.js
+++ TargetBlank.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         新标签页打开链接
-// @version      1.0.0
+// @version      1.0.1
 // @author       X.I.U
 // @description  将网页中所有链接改为新标签页打开~
 // @match        *://*/*
@@ -15,7 +15,20 @@
 
 (function() {
     'use strict';
-    Array.from(document.links).forEach(function (_this) {
-        _this.target = '_blank'
-    })
+    targetBlank(); // 修改为新标签页打开
+
+    // 针对 Discuz! 论坛的帖子
+    if (document.querySelector('meta[name="author"][content*="Discuz!"], meta[name="generator"][content*="Discuz!"]') || document.getElementById('ft') && document.getElementById('ft').textContent.indexOf('Discuz!') > -1) {
+        let atarget = document.getElementById('atarget');
+        if (atarget && atarget.className.indexOf('atarget_1') === -1) { // 强制勾选 [新窗]
+            atarget.click();
+        }
+    }
+
+     // 修改为新标签页打开
+    function targetBlank() {
+        Array.from(document.links).forEach(function (_this) {
+            _this.target = '_blank'
+        })
+    }
 })();
