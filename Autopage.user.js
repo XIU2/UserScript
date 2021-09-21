6f431242fbb439c9a7394a81c8383d47bc7f532d
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-09-21 08:58:30 +0800

    优化 [Github - Stars] 规则

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      2.3.5
+// @version      2.3.6
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有使用「Discuz!、Flarum、DUX(WordPress)」的网站]、百度、谷歌、必应、搜狗、头条、360、微信、贴吧、豆瓣、微博、NGA、V2EX、起点小说、煎蛋网、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、小霸王其乐无穷、CS.RIN.RU、FitGirl、茶杯狐、NO视频、低端影视、奈菲影视、91美剧网、真不卡影院、片库、音范丝、BT之家、爱恋动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、小众软件、极简插件、动漫狂、漫画猫、漫画DB、HiComic、动漫之家、古风漫画网、PubMed、wikiHow、GreasyFork、Github、StackOverflow（以上仅一部分，更多的写不下了...
 // @match        *://*/*
@@ -2100,10 +2100,10 @@
                 pager: {
                     type: 1,
                     nextLink: '//div[@class="paginate-container"]//a[@href][contains(text(), "Next")]',
-                    pageElement: 'css;#js-pjax-container .position-relative .col-lg-12 > div:not(.position-relative):not(.paginate-container)',
+                    pageElement: 'css;#js-pjax-container .position-relative div[class^="col-lg-"] > div:not(.position-relative):not(.paginate-container)',
                     insertPosition: ['css;.paginate-container', 1],
                     replaceE: 'css;.paginate-container',
-                    scrollDelta: 2000
+                    scrollDelta: 2500
                 }
             }, //               Github - 用户 Star 列表
             github_issues: {
