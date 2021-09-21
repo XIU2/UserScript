9cf8e1510cc234a75bc3a772b31f1fa8b1888716
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-09-21 19:46:11 +0800

    优化 [NGA] 规则（适配点击帖子非网页跳转方式）

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      2.3.7
+// @version      2.3.8
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有使用「Discuz!、Flarum、DUX(WordPress)」的网站]、百度、谷歌、必应、搜狗、头条、360、微信、贴吧、豆瓣、微博、NGA、V2EX、起点小说、煎蛋网、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、小霸王其乐无穷、CS.RIN.RU、FitGirl、茶杯狐、NO视频、低端影视、奈菲影视、91美剧网、真不卡影院、片库、音范丝、BT之家、爱恋动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、小众软件、极简插件、动漫狂、漫画猫、漫画DB、HiComic、动漫之家、古风漫画网、PubMed、wikiHow、GreasyFork、Github、StackOverflow（以上仅一部分，更多的写不下了...
 // @match        *://*/*
@@ -486,7 +486,8 @@
                 SiteTypeID: 0,
                 host: ['bbs.nga.cn', 'ngabbs.com', 'nga.178.com', 'g.nga.cn'],
                 iframe: true,
-                functionStart: function() {if (location.pathname === '/thread.php') { // 帖子列表
+                functionStart: function() {locationchange = true;
+                if (location.pathname === '/thread.php') { // 帖子列表
                     curSite = DBSite.nga_thread;
                 } else if (location.pathname === '/read.php') { // 帖子内
                     curSite = DBSite.nga_read;
