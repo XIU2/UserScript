2c36d8098b4ba59ad75289dba93c6240dfb5d44c
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-09-23 09:44:35 +0800

    新增 [屏蔽首页活动广告] 功能

--- Zhihu-Beautification.user.js
+++ Zhihu-Beautification.user.js
@@ -2,7 +2,7 @@
 // @name         知乎美化
 // @version      1.4.0
 // @author       X.I.U
-// @description  宽屏显示、暗黑模式（4种）、暗黑模式跟随浏览器、隐藏文章开头大图、调整图片最大高度、屏蔽首页活动广告、向下翻时自动隐藏顶栏、文章编辑页面与实际文章宽度一致
+// @description  宽屏显示、暗黑模式（4种）、暗黑模式跟随浏览器、屏蔽首页活动广告、隐藏文章开头大图、调整图片最大高度、向下翻时自动隐藏顶栏、文章编辑页面与实际文章宽度一致
 // @match        *://www.zhihu.com/*
 // @match        *://zhuanlan.zhihu.com/*
 // @icon         https://static.zhihu.com/heifetz/favicon.ico
