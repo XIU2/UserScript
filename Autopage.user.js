6bde9e7654093d555032ae5777ceb6d5088176f6
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-09-25 22:24:07 +0800

    新增 [片库] 其他域名

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      2.5.0
+// @version      2.5.1
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有使用「Discuz!、Flarum、DUX(WordPress)」的网站]、百度、谷歌、必应、搜狗、头条、360、微信、贴吧、豆瓣、微博、NGA、V2EX、龙的天空、起点小说、煎蛋网、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、小霸王其乐无穷、CS.RIN.RU、FitGirl、茶杯狐、NO视频、低端影视、奈菲影视、91美剧网、真不卡影院、片库、音范丝、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、极简插件、小众软件、动漫狂、漫画猫、漫画DB、HiComic、动漫之家、古风漫画网、PubMed、wikiHow、GreasyFork、Github、StackOverflow（以上仅一部分，更多的写不下了...
 // @match        *://*/*
@@ -1211,9 +1211,10 @@
                     scrollDelta: 2000
                 }
             },
-            mypianku: {
+            pianku: {
                 SiteTypeID: 0,
-                host: 'www.mypianku.net',
+                host: ['www.pianku.li', 'www.mypianku.net'],
+                functionStart: function() {if (location.pathname != '/') {curSite = DBSite.pianku;}},
                 pager: {
                     type: 1,
                     nextLink: 'css;a.a1[href]',
@@ -1223,7 +1224,7 @@
                     scrollDelta: 1500
                 },
                 function: {
-                    before: mypianku_functionBefore
+                    before: pianku_functionBefore
                 }
             }, //        片库
             cupfox: {
@@ -3296,7 +3297,7 @@
 
 
     // [片库] 的插入前函数（加载图片）
-    function mypianku_functionBefore(pageElems) {
+    function pianku_functionBefore(pageElems) {
         pageElems.forEach(function (one) {
             let now = one.querySelector('img')
             if (now) {
