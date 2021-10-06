e9d2de25467eaabde053b9f8d68e397391e6162e
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-10-06 22:37:09 +0800

    新增 [LowEndTalk] 支持

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      2.7.4
+// @version      2.7.5
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有使用「Discuz!、Flarum、DUX(WordPress)」的网站]、百度、谷歌、必应、搜狗、头条搜索、360 搜索、微信搜索、贴吧、豆瓣、微博、NGA、V2EX、煎蛋网、糗事百科、龙的天空、起点小说、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、FitGirl、片库、茶杯狐、NO视频、低端影视、奈菲影视、91美剧网、真不卡影院、音范丝、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、极简插件、小众软件、动漫狂、漫画猫、漫画DB、HiComic、动漫之家、古风漫画网、PubMed、wikiHow、GreasyFork、Github、StackOverflow（以上仅一部分，更多的写不下了...
 // @match        *://*/*
@@ -951,6 +951,18 @@
                     scrollDelta: 1200
                 }
             }, //     篱笆网论坛 - 搜索页
+            lowendtalk: {
+                SiteTypeID: 0,
+                host: 'lowendtalk.com',
+                pager: {
+                    type: 1,
+                    nextLink: 'css;a.Next[href]',
+                    pageElement: 'css;ul.DataList > li',
+                    insertPosition: ['css;ul.DataList', 3],
+                    replaceE: 'css;.Pager',
+                    scrollDelta: 1500
+                }
+            }, //          LowEndTalk
             lieyou: {
                 SiteTypeID: 0,
                 host: 'bbs.lieyou888.com',
