7f4dd3e1a4411912900a7d7949b77c01e4c419c8
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-10-06 19:40:09 +0800

    新增 [宅男女神] 支持

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      2.7.3
+// @version      2.7.4
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有使用「Discuz!、Flarum、DUX(WordPress)」的网站]、百度、谷歌、必应、搜狗、头条搜索、360 搜索、微信搜索、贴吧、豆瓣、微博、NGA、V2EX、煎蛋网、糗事百科、龙的天空、起点小说、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、FitGirl、片库、茶杯狐、NO视频、低端影视、奈菲影视、91美剧网、真不卡影院、音范丝、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、极简插件、小众软件、动漫狂、漫画猫、漫画DB、HiComic、动漫之家、古风漫画网、PubMed、wikiHow、GreasyFork、Github、StackOverflow（以上仅一部分，更多的写不下了...
 // @match        *://*/*
@@ -3564,6 +3564,65 @@
                     scrollDelta: 1000
                 }
             }, //          壁纸
+            fnvshen: {
+                SiteTypeID: 0,
+                host: 'www.fnvshen.com',
+                functionStart: function() {
+                    if (location.pathname.indexOf('/gallery/') > -1 || location.pathname.indexOf('/tag/') > -1) {
+                        curSite = DBSite.fnvshen;
+                    } else if (location.pathname.indexOf('/g/') > -1) {
+                        curSite = DBSite.fnvshen_g; document.lastElementChild.appendChild(document.createElement('style')).textContent = '.yalayi_box {display: none !important; margin: -4px 0 !important;}';
+                    } else if (/\/article\/\d+\//.test(location.pathname)) {
+                        curSite = DBSite.fnvshen_article;
+                    } else if (location.pathname.indexOf('/article/') > -1) {
+                        curSite = DBSite.fnvshen_article_list;
+                }},
+                pager: {
+                    type: 1,
+                    nextLink: '//div[@class="pagesYY"]//a[@href][contains(text(), "下一页")]',
+                    pageElement: 'css;#listdiv > ul > li',
+                    insertPosition: ['css;#listdiv > ul', 3],
+                    replaceE: 'css;.pagesYY',
+                    scrollDelta: 1000
+                },
+                function: {
+                    before: src_functionBefore,
+                    parameter: [0, 'img[data-original]', 'data-original']
+                }
+            }, //             宅男女神
+            fnvshen_g: {
+                SiteTypeID: 0,
+                pager: {
+                    type: 1,
+                    nextLink: 'id("pages")/a[@href][contains(text(), "下一页")]',
+                    pageElement: 'css;#hgallery > img',
+                    insertPosition: ['css;#hgallery', 3],
+                    replaceE: 'css;#pages',
+                    scrollDelta: 1000
+                }
+            }, //           宅男女神 - 图片内
+            fnvshen_article_list: {
+                SiteTypeID: 0,
+                pager: {
+                    type: 1,
+                    nextLink: '//div[@class="pagesYY"]//a[@href][contains(text(), "下一页")]',
+                    pageElement: 'css;li.other_girlli',
+                    insertPosition: ['//ul[./li[@class="other_girlli"]]', 3],
+                    replaceE: 'css;.pagesYY',
+                    scrollDelta: 1000
+                }
+            }, //宅男女神 - 文章列表
+            fnvshen_article: {
+                SiteTypeID: 0,
+                pager: {
+                    type: 1,
+                    nextLink: 'css;.pagesYY a.cur+a',
+                    pageElement: 'css;#articleDiv',
+                    insertPosition: ['css;#articleDiv', 5],
+                    replaceE: 'css;.pagesYY',
+                    scrollDelta: 1000
+                }
+            }, //     宅男女神 - 文章内
             zhutix: {
                 SiteTypeID: 0,
                 host: 'zhutix.com',
