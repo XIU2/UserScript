8c0f069149bc946a82dd762c3beafa5763548f43
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-10-08 22:00:59 +0800

    新增 [漫岛动漫、吐槽弹幕网] 支持

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      2.7.9
+// @version      2.8.0
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有使用「Discuz!、Flarum、DUX(WordPress)」的网站]、百度、谷歌、必应、搜狗、头条搜索、360 搜索、微信搜索、贴吧、豆瓣、微博、NGA、V2EX、煎蛋网、糗事百科、龙的天空、起点小说、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、FitGirl、片库、茶杯狐、NO视频、低端影视、奈菲影视、91美剧网、真不卡影院、音范丝、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、极简插件、小众软件、动漫狂、漫画猫、漫画DB、HiComic、动漫之家、古风漫画网、PubMed、wikiHow、GreasyFork、Github、StackOverflow（以上仅一部分，更多的写不下了...
 // @match        *://*/*
@@ -1677,6 +1677,25 @@
                     scrollDelta: 1000
                 }
             }, //         樱花动漫 - 搜索页等
+            mandao: {
+                SiteTypeID: 0,
+                host: 'www.mandao.tv',
+                functionStart: function() {if (location.pathname != '/' && location.pathname.indexOf('/man') === -1) {
+                    curSite = DBSite.mandao;
+                }},
+                pager: {
+                    type: 1,
+                    nextLink: '//div[contains(@class, "page")]/a[contains(text(), ">")]',
+                    pageElement: 'css;.index-tj > ul > li',
+                    insertPosition: ['css;.index-tj > ul', 3],
+                    replaceE: 'css;.page',
+                    scrollDelta: 1000
+                },
+                function: {
+                    before: src_functionBefore,
+                    parameter: [0, 'img[data-original]', 'data-original']
+                }
+            }, //        漫岛动漫
             zzzfun: {
                 SiteTypeID: 0,
                 host: 'www.zzzfun.com',
@@ -1714,6 +1733,34 @@
                     parameter: [0, 'img[data-original]', 'data-original']
                 }
             }, // ZzzFun 动漫 - 搜索页
+            tucao: {
+                SiteTypeID: 0,
+                host: 'www.tucao.one',
+                functionStart: function() {if (location.pathname.indexOf('/list/') > -1) {
+                    curSite = DBSite.tucao;
+                } else if (location.search.indexOf('search') > -1) {
+                    curSite = DBSite.tucao_search;
+                }},
+                pager: {
+                    type: 1,
+                    nextLink: 'css;.pagego a',
+                    pageElement: 'css;.list > ul > li',
+                    insertPosition: ['css;.list > ul', 3],
+                    replaceE: 'css;.newpages, .pagego, #float_show',
+                    scrollDelta: 1000
+                }
+            }, //         吐槽弹幕网
+            tucao_search: {
+                SiteTypeID: 0,
+                pager: {
+                    type: 1,
+                    nextLink: '//a[@class="a1"][contains(text(), "下一页")]',
+                    pageElement: 'css;.search_list > *',
+                    insertPosition: ['css;.search_list', 3],
+                    replaceE: 'css;.pages',
+                    scrollDelta: 1000
+                }
+            }, //  吐槽弹幕网 - 搜索页
             yinfans: {
                 SiteTypeID: 0,
                 host: 'www.yinfans.net',
