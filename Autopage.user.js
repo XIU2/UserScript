8c62c7327517a793372240cbdbae6ebd6776fafc
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-10-22 13:58:59 +0800

    优化 [Discuz!] 规则识别问题

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      3.1.4
+// @version      3.1.5
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、DUX/XIU/D8/Begin(WP主题)」网站]、百度、谷歌、必应、搜狗、头条搜索、360 搜索、微信搜索、贴吧、豆瓣、微博、NGA、V2EX、B 站(Bilibili)、蓝奏云、煎蛋网、糗事百科、龙的天空、起点小说、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、FitGirl、片库、茶杯狐、NO视频、低端影视、奈菲影视、91美剧网、音范丝、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、极简插件、小众软件、动漫狂、漫画猫、漫画DB、动漫之家、古风漫画网、PubMed、wikiHow、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @match        *://*/*
@@ -4204,12 +4204,12 @@
 
     // [Discuz! 论坛] 判断各版块帖子列表类型
     function discuzForum() {
-        if (document.querySelector('#pgbtn, .pgbtn')) { //          如果各版块帖子列表已存在这个元素，说明自带了无缝翻页
-            return
-        } else if (document.getElementById('autopbn')) { //         判断是否有 [下一页] 按钮
+        if (document.getElementById('autopbn')) { //         判断是否有 [下一页] 按钮
             curSite = DBSite.discuz_forum;
-        } else if (document.getElementById('waterfall')) { //       判断是否为图片模式
-            curSite = DBSite.discuz_waterfall; waterfallStyle(); // 图片模式列表样式预处理
+        } else if (document.getElementById('waterfall')) { //           判断是否为图片模式
+            if (!document.querySelector('#pgbtn, .pgbtn')) { //         如果各版块帖子列表已存在这个元素，说明自带了无缝翻页
+                curSite = DBSite.discuz_waterfall; waterfallStyle(); // 图片模式列表样式预处理
+            }
         } else {
             curSite = DBSite.discuz_guide;
         }
