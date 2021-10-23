2588068e5f26fd7ef5b81906486d5e065cad2115
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-10-23 14:56:07 +0800

    新增 [御书网] 支持

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      3.1.5
+// @version      3.1.6
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、DUX/XIU/D8/Begin(WP主题)」网站]、百度、谷歌、必应、搜狗、头条搜索、360 搜索、微信搜索、贴吧、豆瓣、微博、NGA、V2EX、B 站(Bilibili)、蓝奏云、煎蛋网、糗事百科、龙的天空、起点小说、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、FitGirl、片库、茶杯狐、NO视频、低端影视、奈菲影视、91美剧网、音范丝、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、极简插件、小众软件、动漫狂、漫画猫、漫画DB、动漫之家、古风漫画网、PubMed、wikiHow、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @match        *://*/*
@@ -2721,6 +2721,46 @@
                     scrollD: 900
                 }
             }, //             宝书网- 手机版
+            yushubo: {
+                host: 'www.yushubo.com',
+                functionStart: function() {if (location.pathname.indexOf('/read_') > -1) {
+                    curSite = DBSite.yushubo;
+                } else if (location.pathname.indexOf('/lists/') > -1) {
+                    curSite = DBSite.yushubo_list;
+                } else if (location.pathname.indexOf('/all') > -1) {
+                    curSite = DBSite.yushubo_all;
+                }},
+                insStyle: '.readbg.mt10 {display: none !important;}',
+                pager: {
+                    type: 1,
+                    nextL: '//div[contains(@class, "articlebtn")]/a[contains(text(), "下一页") or contains(text(), "下一章")]',
+                    pageE: 'css;#BookText',
+                    insertP: ['css;#BookText', 5],
+                    replaceE: 'css;.articlebtn, head > title',
+                    history: true,
+                    scrollD: 1000
+                }
+            }, //               御书网
+            yushubo_list: {
+                pager: {
+                    type: 1,
+                    nextL: 'id("pager")//a[contains(text(), "下一页")]',
+                    pageE: 'css;.books-list > ul > li',
+                    insertP: ['css;.books-list > ul', 3],
+                    replaceE: 'css;#pager',
+                    scrollD: 1000
+                }
+            }, //          御书网 - 分类页
+            yushubo_all: {
+                pager: {
+                    type: 1,
+                    nextL: 'id("pager")//a[contains(text(), "下一页")]',
+                    pageE: 'css;ul.search-list > li',
+                    insertP: ['css;ul.search-list', 3],
+                    replaceE: 'css;#pager',
+                    scrollD: 1000
+                }
+            }, //           御书网 - 书库页
             xineyby: {
                 host: 'www.xineyby.com',
                 functionStart: function() {if (location.pathname.indexOf('/read/') > -1) {
