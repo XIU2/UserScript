be847d9b323db32a696d1143b6660041ee88ae62
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-10-25 09:43:32 +0800

    新增 [秀人美女网] 支持

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      3.1.7
+// @version      3.1.8
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、DUX/XIU/D8/Begin(WP主题)」网站]、百度、谷歌、必应、搜狗、头条搜索、360 搜索、微信搜索、贴吧、豆瓣、微博、NGA、V2EX、B 站(Bilibili)、蓝奏云、煎蛋网、糗事百科、龙的天空、起点小说、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、FitGirl、片库、茶杯狐、NO视频、低端影视、奈菲影视、91美剧网、音范丝、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、极简插件、小众软件、动漫狂、漫画猫、漫画DB、动漫之家、古风漫画网、PubMed、wikiHow、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @match        *://*/*
@@ -4006,6 +4006,44 @@
                     pF: [0, 'img[_src]', '_src']
                 }
             }, //      新片场
+            xrmn5: {
+                host: 'www.xrmn5.com',
+                functionStart: function() {if (/\d+\.html/.test(location.pathname)) {
+                    curSite = DBSite.xrmn5;
+                } else if (location.pathname.indexOf('/search') > -1) {
+                    curSite = DBSite.xrmn5_search;
+                /*} else {
+                    curSite = DBSite.xrmn5_list;*/
+                }},
+                pager: {
+                    type: 1,
+                    nextL: '//div[contains(@class, "page")]//a[contains(text(), "下页")]',
+                    pageE: 'css;.content_left > p > img',
+                    insertP: ['css;.content_left > p', 3],
+                    replaceE: 'css;.page',
+                    scrollD: 3000
+                }
+            }, //               秀人美女网 - 图片页
+            xrmn5_list: {
+                pager: {
+                    type: 1,
+                    nextL: '//div[contains(@class, "page")]//a[contains(text(), "下页")]',
+                    pageE: 'css;ul.update_area_lists > li',
+                    insertP: ['css;ul.update_area_lists', 3],
+                    replaceE: 'css;.page',
+                    scrollD: 2000
+                }
+            }, //          秀人美女网 - 分类页
+            xrmn5_search: {
+                pager: {
+                    type: 1,
+                    nextL: 'css;.page a.current+a',
+                    pageE: 'css;div.sousuo',
+                    insertP: ['//div[contains(@class, "sousuo")][last()]', 4],
+                    replaceE: 'css;.page',
+                    scrollD: 2000
+                }
+            }, //        秀人美女网 - 搜索页
             fnvshen: {
                 host: 'www.fnvshen.com',
                 functionStart: function() {
