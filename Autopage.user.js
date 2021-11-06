55169cfeb358c8e4d4e310c6a6fb1a8dce5c2962
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-11-06 18:29:32 +0800

    更新 [Gitee] 规则

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      3.2.8
+// @version      3.2.9
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、DUX/XIU/D8/Begin(WP主题)」网站]、百度、谷歌、必应、搜狗、头条搜索、360 搜索、微信搜索、贴吧、豆瓣、微博、NGA、V2EX、B 站(Bilibili)、蓝奏云、煎蛋网、糗事百科、龙的天空、起点小说、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、FitGirl、片库、茶杯狐、NO视频、低端影视、奈菲影视、91美剧网、音范丝、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、极简插件、小众软件、动漫狂、漫画猫、漫画DB、动漫之家、古风漫画网、PubMed、wikiHow、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @match        *://*/*
@@ -3388,7 +3388,7 @@
                 functionStart: function() {if (location.search) curSite = DBSite.gitee;},
                 pager: {
                     type: 1,
-                    nextL: 'css;li.next > a',
+                    nextL: 'css;li.next:not(.disabled) > a',
                     pageE: 'css;#hits-list > div',
                     insertP: ['css;#hits-list', 3],
                     replaceE: 'css;ul.pagination',
