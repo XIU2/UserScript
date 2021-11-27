a5586e19b69d5a55c6f3d887d0823e6ab7c405c4
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-11-28 00:15:33 +0800

    新增 [漫画皮] 支持

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      3.7.9
+// @version      3.8.0
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、DUX/XIU/D8/Begin(WP主题)」网站]、百度、谷歌、必应、搜狗、头条搜索、360 搜索、微信搜索、贴吧、豆瓣、知乎、微博、NGA、V2EX、B 站(Bilibili)、Pixiv、蓝奏云、煎蛋网、糗事百科、龙的天空、起点小说、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、片库、茶杯狐、NO视频、低端影视、奈菲影视、音范丝、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、极简插件、小众软件、动漫狂、漫画猫、漫画 DB、动漫之家、拷贝漫画、包子漫画、古风漫画网、Mangabz、PubMed、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @match        *://*/*
@@ -2793,7 +2793,7 @@ function: {
                     curSite = DBSite.manhuatai;
                 } else if (indexOF('/fenlei_') || indexOF('/top_')) {
                     curSite = DBSite.manhuatai_list;
-                    } else if (indexOF('/search')) {
+                } else if (indexOF('/search')) {
                     curSite = DBSite.manhuatai_search;
                 }},
                 history: true,
@@ -2826,6 +2826,31 @@ function: {
                     scrollD: 1500
                 }
             }, //  漫画台 - 搜索页
+            manhuapi: {
+                host: 'www.manhuapi.com',
+                functionS: function() {if (indexOF('/chapter/')) {
+                    curSite = DBSite.manhuapi;
+                } else if (!indexOF(/\/manhua\/\d+\.html/)) {
+                    curSite = DBSite.manhuapi_list;
+                }},
+                history: true,
+                pager: {
+                    type: 1,
+                    nextL: '//div[@class="page"]/a[text()="下一页" or text()="下一章"]',
+                    pageE: 'css;.mh_list img[src]',
+                    replaceE: 'css;.page',
+                    scrollD: 2000
+                }
+            }, //          漫画皮
+            manhuapi_list: {
+                pager: {
+                    type: 1,
+                    nextL: '//div[@class="pages"]/a[text()="下一页"]',
+                    pageE: 'css;.cy_list_mh > ul',
+                    replaceE: 'css;.pages',
+                    scrollD: 1500
+                }
+            }, //     漫画皮 - 分类页
             manhuagui: {
                 host: 'www.mhgui.com',
                 functionS: function() {if (indexOF(/\/comic\/\d+\/\d+\.html/)) {
