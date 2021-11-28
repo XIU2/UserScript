9f02a8be1916e8f81b0a7179d869d840406de483
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-11-28 15:51:27 +0800

    修复 [B站 - 搜索页] 页码太少或最后几页无法翻页的问题

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      3.8.3
+// @version      3.8.4
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、DUX/XIU/D8/Begin(WP主题)」网站]、百度、谷歌、必应、搜狗、头条搜索、360 搜索、微信搜索、贴吧、豆瓣、知乎、微博、NGA、V2EX、B 站(Bilibili)、Pixiv、蓝奏云、煎蛋网、糗事百科、龙的天空、起点小说、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、片库、茶杯狐、NO视频、低端影视、奈菲影视、音范丝、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、极简插件、小众软件、动漫狂、漫画猫、漫画 DB、动漫之家、拷贝漫画、包子漫画、古风漫画网、Mangabz、PubMed、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @match        *://*/*
@@ -5366,11 +5366,12 @@ function: {
     // [bilibili_search] 获取下一页地址
     function bilibili_search_nextL() {
         if (!location.search) return
-        let pageActive = 1, pageLast = parseInt(getCSS('li.page-item.last').innerText);
+        let pageActive = 1, pageLast = parseInt(getXpath('//ul[@class="pages"]/li[contains(@class, "page-item")][not(contains(@class, "next") or contains(@class, "prev"))][last()]').innerText);
+        if (!pageLast) return
         if (indexOF(/page=\d+/, 's')) {
             pageActive = parseInt(/page=\d+/.exec(location.search)[0].replace('page=',''))
         }
-        if (pageActive <= pageLast) {
+        if (pageActive < pageLast) {
             if (indexOF(/page=\d+/, 's')) {
                 return (location.origin + location.pathname + location.search.replace(/page=\d+/,`page=${pageActive+1}`))
             } else {
