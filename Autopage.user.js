64a09997081c65e42aaf9cb4794dc1c8adb040a2
xiu2 <54703944+XIU2@users.noreply.github.com>
2021-12-09 14:49:28 +0800

    补充 [好漫6] 其他域名

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      4.0.8
+// @version      4.0.9
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、DUX/XIU/D8/Begin(WP主题)」网站]、百度、谷歌、必应、搜狗、头条搜索、360 搜索、微信搜索、贴吧、豆瓣、知乎、微博、NGA、V2EX、B 站(Bilibili)、Pixiv、蓝奏云、煎蛋网、糗事百科、龙的天空、起点小说、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、片库、茶杯狐、NO视频、低端影视、奈菲影视、音范丝、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、极简插件、小众软件、动漫狂、漫画猫、漫画 DB、动漫之家、拷贝漫画、包子漫画、古风漫画网、Mangabz、PubMed、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @match        *://*/*
@@ -2567,15 +2567,16 @@ function: {
                 }}
             }, //            漫本
             haoman: {
-                host: 'www.haoman6.com',
+                host: ['www.haoman6.com', 'www.g-lens.com'],
                 functionS: function() {
                     if (indexOF('/chapter/')) {
                         curSite = DBSite.haoman;
+                        if (location.hostname == 'www.g-lens.com') curSite.function.pF = [0, 'img[data-echo]', 'data-echo']
                     } else if (indexOF('/comic/')) {
                         if (getCSS('.chapter__more')) getCSS('.chapter__more').click();
                     } else if (indexOF('/category/') || indexOF('/search')) {
                         curSite = DBSite.haoman_list;
-                    }},
+                }},
                 insStyle: '.rd-guess, .rd-aside, .page-index__btn {display: none !important;} .rd-article__pic {display: initial !important;} .rd-article__pic > img {margin: 0 auto;display: block;height: auto;min-height: 200px;}',
                 history: true,
                 pager: {
