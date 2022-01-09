dcec3312e18d710854f359bd57fad1cc34d7b241
xiu2 <54703944+XIU2@users.noreply.github.com>
2022-01-09 09:27:33 +0800

    新增 [搜小说] 支持

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      4.3.8
+// @version      4.3.9
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、DUX/XIU/D8/Begin(WP主题)」网站]、百度、谷歌、必应、搜狗、头条搜索、360 搜索、微信搜索、贴吧、豆瓣、知乎、微博、NGA、V2EX、B 站(Bilibili)、Pixiv、蓝奏云、煎蛋网、糗事百科、龙的天空、起点小说、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、片库、茶杯狐、NO视频、低端影视、奈菲影视、音范丝、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、极简插件、小众软件、动漫狂、漫画猫、漫画 DB、动漫之家、拷贝漫画、包子漫画、古风漫画网、Mangabz、PubMed、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @match        *://*/*
@@ -352,8 +352,8 @@ function: {
                 pager: {
                     type: 1,
                     nextL: '//a[contains(text(), "下一章") or contains(text(), "下一页")]',
-                    pageE: 'css;#content, #chaptercontent, .chaptercontent, #BookText',
-                    insertP: ['css;#content, #chaptercontent, .chaptercontent, #BookText', 6],
+                    pageE: 'css;#content, .content, #chaptercontent, .chaptercontent, #BookText',
+                    insertP: ['css;#content, .content, #chaptercontent, .chaptercontent, #BookText', 6],
                     replaceE: '//*[./a[contains(text(), "下一章") or contains(text(), "下一页")]]',
                     scrollD: 1500
                 }
@@ -3515,6 +3515,20 @@ function: {
                     scrollD: 1000
                 }
             }, //           御书网 - 书库页
+            soxscc: {
+                host: 'www.soxscc.org',
+                functionS: function() {if (indexOF(/\/\d{4,}\.html/)) {curSite = DBSite.soxscc;}},
+                insStyle: '.content > p, img {display: none !important;}',
+                history: true,
+                pager: {
+                    type: 1,
+                    nextL: '//font[contains(text(), "下一章")]/following-sibling::a[1]',
+                    pageE: 'css;.content',
+                    insertP: ['css;.content', 6],
+                    replaceE: 'css;.pagego',
+                    scrollD: 1500
+                }
+            }, //                搜小说
             xineyby: {
                 host: 'www.xineyby.com',
                 functionS: function() {if (indexOF('/read/')) {
@@ -6808,7 +6822,7 @@ function: {
             console.info('[自动无缝翻页] - 使用 WordPress <D8> 主题的网站'); return 102;
         } else if (getCSS('link[href*="themes/begin" i], script[src*="themes/begin" i], img[src*="themes/begin" i]')) {
             console.info('[自动无缝翻页] - 使用 WordPress <Begin> 主题的网站'); return 103;
-        } else if (getCSS('meta[name="description"][content*="小说"], meta[name="description"][content*="章节"], meta[name="description"][content*="阅读"]') && getCSS('#content, #chaptercontent, .chaptercontent, #BookText') && getXpath('//a[contains(text(), "下一章") or contains(text(), "下一页")]')) {
+        } else if (getCSS('meta[name="description"][content*="小说"], meta[name="description"][content*="章节"], meta[name="description"][content*="阅读"]') && getCSS('#content, .content, #chaptercontent, .chaptercontent, #BookText') && getXpath('//a[contains(text(), "下一章") or contains(text(), "下一页")]')) {
             console.info('[自动无缝翻页] - <笔趣阁> 模板的小说网站'); return 200;
         } else if (self != top) {
             return -1;
