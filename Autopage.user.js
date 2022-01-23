627b0523bfb50f83073e389f86413becbe0dfaaf
xiu2 <54703944+XIU2@users.noreply.github.com>
2022-01-23 19:15:28 +0800

    更新 [秀人集] 规则

--- Autopage.user.js
+++ Autopage.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         自动无缝翻页
-// @version      4.5.4
+// @version      4.5.5
 // @author       X.I.U
 // @description  无缝拼接下一页内容（瀑布流），目前支持：[所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、NexusPHP、DUX/XIU/D8/Begin(WP主题)」网站]、百度、谷歌、必应、搜狗、头条搜索、360 搜索、微信搜索、贴吧、豆瓣、知乎、微博、NGA、V2EX、B 站(Bilibili)、Pixiv、蓝奏云、煎蛋网、糗事百科、龙的天空、起点小说、IT之家、千图网、Pixabay、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、片库、茶杯狐、NO视频、低端影视、奈菲影视、音范丝、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、极简插件、小众软件、动漫狂、漫画猫、漫画 DB、动漫之家、拷贝漫画、包子漫画、Mangabz、PubMed、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @match        *://*/*
@@ -5246,17 +5246,17 @@ function: {
                 }},
                 pager: {
                     type: 1,
-                    nextL: '//div[@class="page"]/a[contains(text(), "后")]',
-                    pageE: 'css;div.img > p > *',
+                    nextL: '//div[@class="page"]/a[text()="下页"]',
+                    pageE: 'css;div.content > p > *',
                     replaceE: 'css;.page',
-                    scrollD: 2000
+                    scrollD: 1500
                 }
             }, //            秀人集 - 图片页
             xiurenji_list: {
                 pager: {
                     type: 1,
-                    nextL: '//div[@class="page"]/a[contains(text(), "下页")]',
-                    pageE: 'css;td >.tp2 > *',
+                    nextL: '//div[@class="page"]/a[text()="下页"]',
+                    pageE: 'css;li.i_list',
                     replaceE: 'css;.page',
                     scrollD: 1000
                 }
@@ -5267,7 +5267,7 @@ function: {
                     nextL: 'css;.page > a.current+a',
                     pageE: 'css;.node > *',
                     replaceE: 'css;.page',
-                    scrollD: 1000
+                    scrollD: 700
                 }
             }, //     秀人集 - 搜索页
             mmtvv: {
