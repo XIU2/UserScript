336dc66c152192de10bbdda16859755536d9cbc5
xiu2 <54703944+XIU2@users.noreply.github.com>
2022-02-26 08:22:37 +0800

    修复 专栏文章页部分功能失效的问题

--- Zhihu-Enhanced.user.js
+++ Zhihu-Enhanced.user.js
@@ -3,10 +3,10 @@
 // @name:zh-CN   知乎增强
 // @name:zh-TW   知乎增強
 // @name:en      Zhihu enhancement
-// @version      2.0.0
+// @version      2.0.1
 // @author       X.I.U
 // @description  移除登录弹窗、屏蔽首页视频、默认收起回答、快捷收起回答/评论（左键两侧）、快捷回到顶部（右键两侧）、屏蔽用户、屏蔽关键词、移除高亮链接、屏蔽盐选内容、净化搜索热门、净化标题消息、展开问题描述、显示问题作者、置顶显示时间、完整问题时间、区分问题文章、直达问题按钮、默认高清原图、默认站外直链
-// @description:zh-TW  移除登錄彈窗、屏蔽首頁視頻、默認收起回答、快捷收起回答/評論（左鍵兩側）、快捷回到頂部（右鍵兩側）、屏蔽用戶、屏蔽關鍵詞、移除高亮鏈接、屏蔽鹽選內容、淨化搜索熱門、淨化標題消息、展開問題描述、顯示問題作者、置頂顯示時間、完整問題時間、區分問題文章、直達問題按鈕、默認高清原圖、默認站外直鏈
+// @description:zh-TW  移除登錄彈窗、屏蔽首頁視頻、默認收起回答、快捷收起回答/評論、快捷回到頂部、屏蔽用戶、屏蔽關鍵詞、移除高亮鏈接、屏蔽鹽選內容、淨化搜索熱門、淨化標題消息、置頂顯示時間、完整問題時間、區分問題文章、默認高清原圖、默認站外直鏈...
 // @description:en  A more personalized Zhihu experience~
 // @match        *://www.zhihu.com/*
 // @match        *://zhuanlan.zhihu.com/*
@@ -1389,9 +1389,11 @@ function questionInvitation(){
     }
 
     function start(){
-        cleanSearch(); //                                                      净化搜索热门
         removeHighlightLink(); //                                              移除高亮链接
-        if (location.hostname != 'zhuanlan.zhihu.com') {collapsedAnswer();} // 一键收起回答
+        if (location.hostname != 'zhuanlan.zhihu.com') {
+            if (location.pathname.indexOf('/column/') === -1) cleanSearch(); //净化搜索热门
+            collapsedAnswer(); //                                              一键收起回答
+        }
         closeFloatingComments(); //                                            快捷关闭悬浮评论（监听点击事件，点击网页两侧空白处）
         blockKeywords('comment'); //                                           屏蔽指定关键词（评论）
 
