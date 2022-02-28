81142d92d6bf0ecbf1af916c6d901e86f43e575d
xiu2 <54703944+XIU2@users.noreply.github.com>
2022-02-28 20:25:12 +0800

    优化 模式 3 虎牙直播显示效果

--- DarkMode.user.js
+++ DarkMode.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   护眼模式
 // @name:zh-TW   護眼模式
 // @name:en      Dark Mode
-// @version      1.4.0
+// @version      1.4.1
 // @author       X.I.U
 // @description  简单有效的全网通用护眼模式（夜间模式、暗黑模式、深色模式）
 // @description:zh-CN  简单有效的全网通用护眼模式（夜间模式、暗黑模式、深色模式）
@@ -392,7 +392,11 @@
                 break;
             case 3:
                 style += style_31;
-                if (location.hostname.indexOf('.bilibili.com') > -1) {style += `#bilibiliPlayer video {filter: none !important;} #bilibiliPlayer:not(.mode-fullscreen), #bilibiliPlayer:not(.mode-fullscreen) .bilibili-player-video-sendbar {filter: invert(1) !important;} .bilibili-player.bilibili-player-area-v1, .bilibili-player.bilibili-player-area-v3, #bilibiliPlayer {box-shadow: none !important;} * {font-weight: bold !important;}`}
+                if (location.hostname.indexOf('.bilibili.com') > -1) {
+                    style += `#bilibiliPlayer video {filter: none !important;} #bilibiliPlayer:not(.mode-fullscreen), #bilibiliPlayer:not(.mode-fullscreen) .bilibili-player-video-sendbar {filter: invert(1) !important;} .bilibili-player.bilibili-player-area-v1, .bilibili-player.bilibili-player-area-v3, #bilibiliPlayer {box-shadow: none !important;} * {font-weight: bold !important;}`
+                } else if (location.hostname.indexOf('.huya.com') > -1) {
+                    style += `#player-wrap[style="height: 100%;"], .player-loading, .sidebar-show, #player-ctrl-wrap {filter: invert(1) !important;}`
+                }
                 break;
         }
         style_Add.id = 'XIU2DarkMode';
