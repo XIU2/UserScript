675c2165ce397945cd28970c8784921c5ee88ee9
xiu2 <54703944+XIU2@users.noreply.github.com>
2022-03-14 17:57:42 +0800

    新增 [字幕库]、[Javbooks]、[JAV Library] 支持; 补充 [anix.moe] 域名; 优化 [所有 Xiuno 论坛] 通用规则

--- Autopage.user.js
+++ Autopage.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   自动无缝翻页
 // @name:zh-TW   自動無縫翻頁
 // @name:en      AutoPager
-// @version      5.3.4
+// @version      5.3.5
 // @author       X.I.U
 // @description  ⭐无缝衔接下一页内容到网页底部（类似瀑布流）⭐，目前支持：【所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、NexusPHP...」论坛】【百度、谷歌(Google)、必应(Bing)、搜狗、微信、360、Yahoo、Yandex 等搜索引擎...】、贴吧、豆瓣、知乎、微博、NGA、V2EX、B 站(Bilibili)、煎蛋网、龙的天空、起点中文、千图网、千库网、Pixabay、Pixiv、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、RuTracker、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、小众软件、【动漫狂、漫画猫、漫画屋、漫画 DB、动漫之家、拷贝漫画、HiComic、Mangabz、Xmanhua 等漫画网站...】、PubMed、Z-Library、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @description:zh-TW  ⭐無縫銜接下一頁內容到網頁底部（類似瀑布流）⭐，支持各論壇、社交、遊戲、漫畫、小說、學術、搜索引擎(Google、Bing、Yahoo...) 等網站~
@@ -518,7 +518,7 @@ function: {
             xiuno: {
                 url: ()=> {if (lp == '/' || indexOF(/\/(index|forum)/)) {curSite = DBSite.xiuno;} else if (indexOF('/thread')) {curSite = DBSite.xiuno_post;}},
                 pager: {
-                    nextL: '//li[@class="page-item"]/a[text()="▶"]',
+                    nextL: '//li[@class="page-item"]/a[text()="▶" or text()="»" or contains(text(),">") or contains(text(),"下一页")]',
                     pageE: 'ul.threadlist > li',
                     replaceE: 'ul.pagination'
                 }
@@ -526,7 +526,7 @@ function: {
             xiuno_post: {
                 thread: true,
                 pager: {
-                    nextL: '//li[@class="page-item"]/a[text()="▶"]',
+                    nextL: '//li[@class="page-item"]/a[text()="▶" or text()="»" or contains(text(),">") or contains(text(),"下一页")]',
                     pageE: 'li.post[data-pid]:not(.newpost)',
                     replaceE: 'ul.pagination'
                 }
@@ -2523,6 +2523,7 @@ function: {
         if (curSite.pager.type === undefined) curSite.pager.type = 1; // 默认翻页模式 1
         if (curSite.pager.scrollD === undefined) curSite.pager.scrollD = 1500; // 默认翻页触发线 1500
         if (curSite.pager.interval === undefined) curSite.pager.interval = 500; // 默认间隔时间 500ms
+        //console.log(curSite)
         curSite.pageUrl = ''; // 下一页URL
         windowScroll(function (direction, e) {
             // 下滑 且 未暂停翻页 且 SiteTypeID > 0 时，才准备翻页
