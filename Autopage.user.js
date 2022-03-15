de678e190abf4a3b4551d4d5f57ca69438d2c48d
xiu2 <54703944+XIU2@users.noreply.github.com>
2022-03-15 20:15:37 +0800

    新增 [喵御宅]、[Gelbooru]、[Danbooru] 支持; 更新 [腾龙工作室] 规则; 优化 [部分自带 自动无缝翻页 的网站] 通用规则

--- Autopage.user.js
+++ Autopage.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   自动无缝翻页
 // @name:zh-TW   自動無縫翻頁
 // @name:en      AutoPager
-// @version      5.3.5
+// @version      5.3.6
 // @author       X.I.U
 // @description  ⭐无缝衔接下一页内容到网页底部（类似瀑布流）⭐，目前支持：【所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、NexusPHP...」论坛】【百度、谷歌(Google)、必应(Bing)、搜狗、微信、360、Yahoo、Yandex 等搜索引擎...】、贴吧、豆瓣、知乎、微博、NGA、V2EX、B 站(Bilibili)、煎蛋网、龙的天空、起点中文、千图网、千库网、Pixabay、Pixiv、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、RuTracker、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、小众软件、【动漫狂、漫画猫、漫画屋、漫画 DB、动漫之家、拷贝漫画、HiComic、Mangabz、Xmanhua 等漫画网站...】、PubMed、Z-Library、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @description:zh-TW  ⭐無縫銜接下一頁內容到網頁底部（類似瀑布流）⭐，支持各論壇、社交、遊戲、漫畫、小說、學術、搜索引擎(Google、Bing、Yahoo...) 等網站~
@@ -217,8 +217,10 @@
             console.info('[自动无缝翻页] - <XenForo> 论坛'); return 6;
         } else if (getCSS('head meta[name="generator" i][content="nexusphp" i]') || getXpath('id("footer")[contains(string(), "NexusPHP")]')) {
             console.info('[自动无缝翻页] - <NexusPHP> 论坛'); return 7;
-        } else if (getAllCSS('.load-more, #loadmore, #load-more, .show_more').length === 1) {
-            console.info('[自动无缝翻页] - 部分自带 自动无缝翻页 的网站'); return 8;
+        } else if (getAllCSS('.load-more, .loadmore, #load-more, #loadmore, .show_more').length === 1) {
+            console.info('[自动无缝翻页] - 部分自带 自动无缝翻页 的网站 1'); return 8;
+        } else if (getAllXpath('//*[@onclick][text()="加载更多" or text()="查看更多"]').length === 1) {
+            console.info('[自动无缝翻页] - 部分自带 自动无缝翻页 的网站 2'); return 9;
         } else if (getCSS('link[href*="themes/dux" i], script[src*="themes/dux" i]')) {
             console.info('[自动无缝翻页] - 使用 WordPress <DUX> 主题的网站'); return 100;
         } else if (getCSS('link[href*="themes/xiu" i], script[src*="themes/xiu" i]')) {
@@ -260,8 +262,10 @@
                     DBSite.xenforo.url(); break;
                 case 7: //   < 所有 NexusPHP 论坛 >
                     DBSite.nexusphp.url(); break;
-                case 8: // < 部分自带 自动无缝翻页 的网站 >
-                    curSite = DBSite.loadmore; break;
+                case 8: // < 部分自带 自动无缝翻页 的网站 1 >
+                    DBSite.loadmore.url(); break;
+                case 9: // < 部分自带 自动无缝翻页 的网站 2 >
+                    DBSite.loadmore.url('//*[@onclick][text()="加载更多" or text()="查看更多"]'); break;
                 case 100: // < 所有使用 WordPress DUX 主题的网站 >
                     DBSite.dux.url(); if (location.hostname === 'apphot.cc') {curSite.pager.scrollD = 2500;}; break;
                 case 101: // < 所有使用 WordPress XIU 主题的网站 >
@@ -357,9 +361,9 @@ function: {
     */ //<<< 规则简单说明 >>>
         DBSite = {
             loadmore: {
+                url: function(nextL = '.load-more, .loadmore, #load-more, #loadmore, .show_more') {curSite = DBSite.loadmore; curSite.pager.nextL = nextL;},
                 pager: {
                     type: 2,
-                    nextL: '.load-more, #loadmore, #load-more, .show_more',
                     isHidden: true,
                     interval: 1000,
                     scrollD: 2500
@@ -583,7 +587,6 @@ function: {
             wp_article: {
                 url: function(nextL = 'a.next') {if (!indexOF('/post/') && !getCSS('#comments, .comments-area, #disqus_thread')) {curSite = DBSite.wp_article; curSite.pager.nextL = nextL; if (getCSS('img[data-src]')) {curSite.function = {bF: "return fun.src_bF(pageE, [0, 'img[data-src]', 'data-src'])"};} else if (getCSS('img[data-original]')) {curSite.function = {bF: "return fun.src_bF(pageE, [0, 'img[data-original]', 'data-original'])"};}}},
                 pager: {
-                    nextL: 'a.next',
                     pageE: 'article',
                     replaceE: '.pagination, .paging-navigation',
                     scrollD: 2000
@@ -3332,6 +3335,7 @@ function: {
             "nextL": "#autopbn",
             "nextText": "下一页",
             "nextTextOf": "下一页",
+            "isHidden": true,
             "interval": 1000,
             "scrollD": 1500
         }
