359249625fff369c8c4be3aa1ceeffe5bdd6e5a5
xiu2 <54703944+XIU2@users.noreply.github.com>
2022-03-29 12:37:52 +0800

    优化 首页切换选项卡时功能代码

--- Zhihu-Enhanced.user.js
+++ Zhihu-Enhanced.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   知乎增强
 // @name:zh-TW   知乎增強
 // @name:en      Zhihu enhancement
-// @version      2.0.3
+// @version      2.0.4
 // @author       X.I.U
 // @description  移除登录弹窗、屏蔽首页视频、默认收起回答、快捷收起回答/评论（左键两侧）、快捷回到顶部（右键两侧）、屏蔽用户、屏蔽关键词、移除高亮链接、屏蔽盐选内容、净化搜索热门、净化标题消息、展开问题描述、显示问题作者、置顶显示时间、完整问题时间、区分问题文章、直达问题按钮、默认高清原图、默认站外直链
 // @description:zh-TW  移除登錄彈窗、屏蔽首頁視頻、默認收起回答、快捷收起回答/評論、快捷回到頂部、屏蔽用戶、屏蔽關鍵詞、移除高亮鏈接、屏蔽鹽選內容、淨化搜索熱門、淨化標題消息、置頂顯示時間、完整問題時間、區分問題文章、默認高清原圖、默認站外直鏈...
@@ -1378,6 +1378,10 @@ function questionInvitation(){
                 blockUsers('question'); //                                     屏蔽指定用户
                 blockYanXuan(); //                                             屏蔽盐选内容
             }, 300);
+        } else if (location.pathname == '/') {
+            blockUsers('index'); //                                        屏蔽指定用户
+            blockKeywords('index'); //                                     屏蔽指定关键词
+            blockType(); //                                                屏蔽指定类别（视频/文章等）
         }
     })
 
