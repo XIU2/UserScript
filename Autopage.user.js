c91795f2670d81071233d6525722976b80e7210b
xiu2 <54703944+XIU2@users.noreply.github.com>
2022-03-29 22:03:02 +0800

    优化 获取下一页失败时的代码逻辑

--- Autopage.user.js
+++ Autopage.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   自动无缝翻页
 // @name:zh-TW   自動無縫翻頁
 // @name:en      AutoPager
-// @version      5.5.4
+// @version      5.5.5
 // @author       X.I.U
 // @description  ⭐无缝衔接下一页内容到网页底部（类似瀑布流）⭐，目前支持：【所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、NexusPHP...」论坛】【百度、谷歌(Google)、必应(Bing)、搜狗、微信、360、Yahoo、Yandex 等搜索引擎...】、贴吧、豆瓣、知乎、微博、NGA、V2EX、煎蛋网、龙的天空、起点中文、千图网、千库网、Pixabay、Pixiv、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、RuTracker、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、小众软件、【动漫狂、漫画猫、漫画屋、漫画 DB、动漫之家、拷贝漫画、HiComic、Mangabz、Xmanhua 等漫画网站...】、PubMed、Z-Library、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @description:zh-TW  ⭐無縫銜接下一頁內容到網頁底部（類似瀑布流）⭐，支持各論壇、社交、遊戲、漫畫、小說、學術、搜索引擎(Google、Bing、Yahoo...) 等網站~
@@ -2917,8 +2917,10 @@ function: {
             if (curSite.retry) {
                 console.warn('[自动无缝翻页] 获取主体元素失败，尝试重新获取...')
                 setTimeout(function(){curSite.pageUrl = '';}, curSite.retry)
-            } else { // 尝试替换元素看能不能继续翻页下去
-                if (curSite.pager.replaceE) {
+            } else {
+                console.error('[自动无缝翻页] 获取主体元素失败...')
+                // 尝试替换元素看能不能继续翻页下去
+                /*if (curSite.pager.replaceE) {
                     if (replaceElems(response)) { // 如果替换成功
                         console.log('[自动无缝翻页] 获取主体元素失败，尝试替换元素成功！')
                         // 当前页码 + 1
@@ -2930,7 +2932,7 @@ function: {
                             if (curSite.history) addHistory(response);
                         }
                     } else {console.error('[自动无缝翻页] 获取主体元素失败，尝试替换元素失败...')}
-                }
+                }*/
             }
         }
     }
