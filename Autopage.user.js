48d210087db0c3ac5e927ba3c59e9cb645cd29d0
xiu2 <54703944+XIU2@users.noreply.github.com>
2022-04-02 22:01:25 +0800

    新增 [笔趣阁 - 手机版] 通用规则

--- Autopage.user.js
+++ Autopage.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   自动无缝翻页
 // @name:zh-TW   自動無縫翻頁
 // @name:en      AutoPager
-// @version      5.5.8
+// @version      5.5.9
 // @author       X.I.U
 // @description  ⭐无缝衔接下一页内容到网页底部（类似瀑布流）⭐，目前支持：【所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、NexusPHP...」论坛】【百度、谷歌(Google)、必应(Bing)、搜狗、微信、360、Yahoo、Yandex 等搜索引擎...】、贴吧、豆瓣、知乎、微博、NGA、V2EX、煎蛋网、龙的天空、起点中文、千图网、千库网、Pixabay、Pixiv、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、RuTracker、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、小众软件、【动漫狂、漫画猫、漫画屋、漫画 DB、动漫之家、拷贝漫画、HiComic、Mangabz、Xmanhua 等漫画网站...】、PubMed、Z-Library、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @description:zh-TW  ⭐無縫銜接下一頁內容到網頁底部（類似瀑布流）⭐，支持各論壇、社交、遊戲、漫畫、小說、學術、搜索引擎(Google、Bing、Yahoo...) 等網站~
@@ -264,10 +264,12 @@
 
         } else if (getCSS('.content > #content') && getCSS('.page_chapter') && getXpath('//div[@class="page_chapter"]//a[text()="下一章"]')) {
             console.info(`[自动无缝翻页] - <笔趣阁 1> 模板的小说网站`); return 200;
+        } else if (getCSS('#nr1') && getCSS('.nr_page') && getCSS('#pb_next')) {
+            console.info(`[自动无缝翻页] - <笔趣阁 2 - 手机版> 模板的小说网站`); return 201;
         } else if (getCSS('#txt, .txt') && getCSS('#pb_next, .url_next') && getCSS('.chapter-control, .chapter-page-btn')) {
-            console.info(`[自动无缝翻页] - <笔趣阁 2> 模板的小说网站`); return 201;
-        } else if ((getCSS('meta[name="description" i][content*="小说"], meta[name="description" i][content*="章节"], meta[name="description" i][content*="阅读"]') || location.hostname.indexOf('biqu') > -1 || document.title.indexOf('笔趣阁') > -1 || document.title.indexOf('小说') > -1) && (getCSS('[id="content" i], [class~="content" i], [id="chaptercontent" i], [class~="chaptercontent" i], [id="booktext" i], [id="txtcontent" i]') && getXpath('//a[contains(text(), "下一章") or contains(text(), "下一页")]'))) {
             console.info(`[自动无缝翻页] - <笔趣阁 3> 模板的小说网站`); return 202;
+        } else if ((getCSS('meta[name="description" i][content*="小说"], meta[name="description" i][content*="章节"], meta[name="description" i][content*="阅读"]') || location.hostname.indexOf('biqu') > -1 || document.title.indexOf('笔趣阁') > -1 || document.title.indexOf('小说') > -1) && (getCSS('[id="content" i], [class~="content" i], [id="chaptercontent" i], [class~="chaptercontent" i], [id="booktext" i], [id="txtcontent" i]') && getXpath('//a[contains(text(), "下一章") or contains(text(), "下一页")]'))) {
+            console.info(`[自动无缝翻页] - <笔趣阁 4> 模板的小说网站`); return 203;
         }
         return 0;
     }
@@ -305,9 +307,11 @@
                     curSite = DBSite.typecho_handsome; break;
                 case 200: // < 所有使用 笔趣阁 1 模板的小说网站 >
                     DBSite.biquge1.url(); break;
-                case 201: // < 所有使用 笔趣阁 2 模板的小说网站 >
-                    DBSite.biquge2.url(); break;
+                case 201: // < 所有使用 笔趣阁 2 - 手机版 模板的小说网站 >
+                    curSite = DBSite.biquge1_m; break;
                 case 202: // < 所有使用 笔趣阁 3 模板的小说网站 >
+                    DBSite.biquge2.url(); break;
+                case 203: // < 所有使用 笔趣阁 4 模板的小说网站 >
                     curSite = DBSite.biquge3; break;
                 case 300: // < 部分影视网站 >
                     curSite = DBSite.yingshi; break;
@@ -429,7 +433,17 @@ function: {
                     bF: xs_bF,
                     bFp: [/<br>.{0,10}秒记住.+$/, '']
                 }
-            }, //             笔趣阁 模板的小说网站
+            }, //            笔趣阁 1 模板的小说网站
+            biquge1_m: {
+                style: 'img, .posterror, .show-app2, a[href*="posterror()"], [onclick*="location.href"], [style*="url("], #nr1 > *:not(br) {display: none !important;}',
+                history: true,
+                pager: {
+                    nextL: '#pb_next',
+                    pageE: '#nr1',
+                    insertP: ['#nr1', 6],
+                    replaceE: '.nr_page'
+                }
+            }, //          笔趣阁 2 - 手机版 模板的小说网站
             biquge2: {
                 url: ()=> {if (isMobile() || getCSS('.chapter-page-btn') != null) {curSite = DBSite.biquge2_m;} else {curSite = DBSite.biquge2;}},
                 style: 'img, .posterror, a[href*="posterror()"], [style*="url("], #txt > *:not(br) {display: none !important;}',
@@ -443,7 +457,7 @@ function: {
                     loadTime: 1500,
                     scrollD: 3500
                 }
-            }, //            笔趣阁 2 模板的小说网站
+            }, //            笔趣阁 3 模板的小说网站
             biquge2_m: {
                 style: 'img, .posterror, a[href*="posterror()"], [style*="url("], #txt > *:not(br) {display: none !important;}',
                 history: true,
@@ -454,7 +468,7 @@ function: {
                     replaceE: '.chapter-control, .chapter-page-btn',
                     scrollD: 2000
                 }
-            }, //          笔趣阁 2 模板的小说网站
+            }, //          笔趣阁 3 - 手机版 模板的小说网站
             biquge3: {
                 style: 'img, .posterror, a[href*="posterror()"], [style*="url("], script+div[style="padding:15px;"] {display: none !important;}',
                 history: true,
@@ -469,7 +483,7 @@ function: {
                     bF: xs_bF,
                     bFp: [/<br>.{0,10}秒记住.+$/, '']
                 }
-            }, //             笔趣阁 模板的小说网站
+            }, //            笔趣阁 4 模板的小说网站
             yingshi: {
                 style: 'div.stui-page__all {display: none !important;}',
                 blank: 3,
