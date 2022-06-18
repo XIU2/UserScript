53e0700ec0e3dbf127afb05fcd6935910889820b
xiu2 <54703944+XIU2@users.noreply.github.com>
2022-06-19 06:21:11 +0800

    优化 [部分 WordPress 网站] 通用规则

--- Autopage.user.js
+++ Autopage.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   自动无缝翻页
 // @name:zh-TW   自動無縫翻頁
 // @name:en      AutoPager
-// @version      6.0.10
+// @version      6.0.11
 // @author       X.I.U
 // @description  ⭐无缝加载 下一页内容 至网页底部（类似瀑布流）⭐，目前支持：【所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、NexusPHP...」论坛】【百度、谷歌(Google)、必应(Bing)、搜狗、微信、360、Yahoo、Yandex 等搜索引擎...】、贴吧、豆瓣、知乎、B 站(bilibili)、NGA、V2EX、煎蛋网、龙的天空、起点中文、千图网、千库网、Pixabay、Pixiv、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、RuTracker、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、小众软件、【动漫狂、动漫屋、漫画猫、漫画屋、漫画 DB、动漫之家、拷贝漫画、HiComic、Mangabz、Xmanhua 等漫画网站...】、PubMed、Z-Library、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @description:zh-TW  ⭐無縫加載 下一頁內容 至網頁底部（類似瀑布流）⭐，支持各論壇、社交、遊戲、漫畫、小說、學術、搜索引擎(Google、Bing、Yahoo...) 等網站~
@@ -480,9 +480,9 @@ function: {
                 style: 'img[data-src], img[data-original] {opacity: 1 !important;}',
                 blank: 3,
                 pager: {
-                    type: 3,
                     replaceE: '#nav-below, nav.navigation, nav.paging-navigation, .pagination, .wp-pagenavi, .pagenavi, nav[role="navigation"]',
-                    forceHTTPS: true
+                    forceHTTPS: true,
+                    scrollD: 3000
                 },
                 function: {
                     bF: src_bF
@@ -493,7 +493,7 @@ function: {
                     type: 3,
                     nextL: '.post-page-numbers.current+a',
                     replaceE: '//a[contains(@class,"post-page-numbers")]/parent::*',
-                    scrollD: 2000
+                    scrollD: 3000
                 },
                 function: {
                     bF: src_bF
@@ -505,7 +505,7 @@ function: {
                     nextL: 'li.next > a',
                     pageE: '.blog-post, .post-list',
                     replaceE: '.page-navigator',
-                    scrollD: 2000
+                    scrollD: 3000
                 }
             }, //   部分使用 Typecho 的网站 (handsome)
             biquge1: {
