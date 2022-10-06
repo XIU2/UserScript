058ac1a1a66a5f375b4aa4b5b167919782ffee49
xiu2 <54703944+XIU2@users.noreply.github.com>
2022-10-06 10:36:23 +0800

    新增 [左键双击网页空白处暂停翻页] 时 [显示当前页码及点击暂停翻页] 的页码数字也会变为红色+斜体

--- Autopage.user.js
+++ Autopage.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   自动无缝翻页
 // @name:zh-TW   自動無縫翻頁
 // @name:en      AutoPager
-// @version      6.3.5
+// @version      6.3.6
 // @author       X.I.U
 // @description  ⭐无缝加载 下一页内容 至网页底部（类似瀑布流）⭐，目前支持：【所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、NexusPHP...」论坛】【百度、谷歌(Google)、必应(Bing)、搜狗、微信、360、Yahoo、Yandex 等搜索引擎...】、贴吧、豆瓣、知乎、B 站(bilibili)、NGA、V2EX、煎蛋网、龙的天空、起点中文、千图网、千库网、Pixabay、Pixiv、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、RuTracker、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、小众软件、【动漫狂、动漫屋、漫画猫、漫画屋、漫画 DB、动漫之家、拷贝漫画、HiComic、Mangabz、Xmanhua 等漫画网站...】、PubMed、Z-Library、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @description:zh-TW  ⭐無縫加載 下一頁內容 至網頁底部（類似瀑布流）⭐，支持各論壇、社交、遊戲、漫畫、小說、學術、搜索引擎(Google、Bing、Yahoo...) 等網站~
@@ -18,7 +18,6 @@
 // @connect      raw.iqiq.io
 // @connect      ghproxy.net
 // @connect      github.moeyy.xyz
-// @connect      raw.njuu.cf
 // @connect      raw.kgithub.com
 // @connect      cdn.staticaly.com
 // @connect      gcore.jsdelivr.net
@@ -99,7 +98,6 @@
         'https://raw.kgithub.com/XIU2/UserScript/master/other/Autopage/rules.json',
         'https://ghproxy.net/https://raw.githubusercontent.com/XIU2/UserScript/master/other/Autopage/rules.json',
         'https://github.moeyy.xyz/https://raw.githubusercontent.com/XIU2/UserScript/master/other/Autopage/rules.json',
-        'https://raw.njuu.cf/XIU2/UserScript/master/other/Autopage/rules.json',
         'https://cdn.staticaly.com/gh/XIU2/UserScript/master/other/Autopage/rules.json',
         'https://gcore.jsdelivr.net/gh/XIU2/UserScript/other/Autopage/rules.json',
         'https://fastly.jsdelivr.net/gh/XIU2/UserScript/other/Autopage/rules.json',
@@ -113,8 +111,7 @@
         'https://raw.iqiq.io/XIU2/UserScript/master/other/Autopage/rules.json',
         'https://raw.kgithub.com/XIU2/UserScript/master/other/Autopage/rules.json',
         'https://ghproxy.net/https://raw.githubusercontent.com/XIU2/UserScript/master/other/Autopage/rules.json',
-        'https://github.moeyy.xyz/https://raw.githubusercontent.com/XIU2/UserScript/master/other/Autopage/rules.json',
-        'https://raw.njuu.cf/XIU2/UserScript/master/other/Autopage/rules.json'
+        'https://github.moeyy.xyz/https://raw.githubusercontent.com/XIU2/UserScript/master/other/Autopage/rules.json'
     ],
         loadMoreExclude1 = ['stackoverflow.com'],
         loadMoreExclude2 = ['.smzdm.com','.steampowered.com','.zcool.com.cn','.baidu.com'],
@@ -2628,8 +2625,12 @@ function: {
         if (curSite.pager && curSite.pager.type == 5) window.top.document.xiu_pausePage = pausePage
         document.body.addEventListener('dblclick', function () {
             if (pausePage) {GM_notification({text: `❌ 已暂停本页 [自动无缝翻页]\n    （再次双击可恢复）`, timeout: 2000});} else {GM_notification({text: `✅ 已恢复本页 [自动无缝翻页]\n    （再次双击可暂停）`, timeout: 2000});}
-            pausePage = !pausePage;
-            if (curSite.pager && curSite.pager.type == 5) window.top.document.xiu_pausePage = pausePage
+            if (getCSS('#Autopage_number')) { // 如果同时开启了左下角页码功能，则模拟点击（为了数字变成红色+斜体）
+                getCSS('#Autopage_number').click();
+            } else {
+                pausePage = !pausePage;
+                if (curSite.pager && curSite.pager.type == 5) window.top.document.xiu_pausePage = pausePage
+            }
         });
     }
     // 自定义翻页规则
