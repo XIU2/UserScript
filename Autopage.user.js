1ae43bd0956a3cf0895cda2c924bbd2a122bd86d
xiu2 <54703944+XIU2@users.noreply.github.com>
2023-02-03 09:29:06 +0800

    优化 inherits 翻页规则(现在支持单独新增修改 pager 下某个规则)

--- Autopage.user.js
+++ Autopage.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   自动无缝翻页
 // @name:zh-TW   自動無縫翻頁
 // @name:en      AutoPager
-// @version      6.4.11
+// @version      6.4.12
 // @author       X.I.U
 // @description  ⭐无缝加载 下一页内容 至网页底部（类似瀑布流）⭐，目前支持：【所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、NexusPHP...」论坛】【百度、谷歌(Google)、必应(Bing)、搜狗、微信、360、Yahoo、Yandex 等搜索引擎...】、贴吧、豆瓣、知乎、B 站(bilibili)、NGA、V2EX、煎蛋网、龙的天空、起点中文、千图网、千库网、Pixabay、Pixiv、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、RuTracker、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、小众软件、【动漫狂、动漫屋、漫画猫、漫画屋、漫画 DB、动漫之家、拷贝漫画、HiComic、Mangabz、Xmanhua 等漫画网站...】、PubMed、Z-Library、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @description:zh-TW  ⭐無縫加載 下一頁內容 至網頁底部（類似瀑布流）⭐，支持各論壇、社交、遊戲、漫畫、小說、學術、搜索引擎(Google、Bing、Yahoo...) 等網站~
@@ -1120,7 +1120,11 @@ function: {
                 b = GM_getValue('menu_rules', {}), b1 = Object.keys(b)
             for (let i = 0; i < a1.length; i++) { // 循环 [自定义规则-对象名] 数组
                 if (b1.indexOf(a1[i]) != -1) { // 在 [外置规则-对象名] 数组中，寻找是否有同名的 [自定义规则-对象名]
-                    if (a[a1[i]].inherits === true){ a[a1[i]] = Object.assign({}, b[a1[i]], a[a1[i]]);} // 如果该同名的自定义规则对象含有 inherits 继承标识，则将同名的两者合并（自定义覆盖外置）
+                    if (a[a1[i]].inherits === true){ // 如果该同名的自定义规则对象含有 inherits 继承标识，则将同名的两者合并（自定义 覆盖 外置）
+                        // 如果自定义规则中包含 "pager":{} 规则，则需要先合并 pager 后再去合并整体规则（否则 pager 会被自定义规则完整覆盖）
+                        if (a[a1[i]].pager != undefined && b[a1[i]].pager != undefined) {a[a1[i]].pager = Object.assign({}, b[a1[i]].pager, a[a1[i]].pager)}
+                        a[a1[i]] = Object.assign({}, b[a1[i]], a[a1[i]]);
+                    }
                     delete b[a1[i]] // 删除外置规则中的同名，这样后续合并时，外置规则才不会覆盖自定义规则的同名规则
                 };
             }
