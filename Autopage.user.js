ede0f6ff8612e28245eae357fc1a30a09e9eaa70
xiu2 <54703944+XIU2@users.noreply.github.com>
2024-07-04 11:41:47 +0800

    优化 [XenForo 论坛] 通用规则匹配方式

--- Autopage.user.js
+++ Autopage.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   自动无缝翻页
 // @name:zh-TW   自動無縫翻頁
 // @name:en      AutoPager
-// @version      6.6.8
+// @version      6.6.9
 // @author       X.I.U
 // @description  ⭐无缝加载 下一页内容 至网页底部（类似瀑布流）⭐，目前支持：【所有「Discuz!、Flarum、phpBB、Xiuno、XenForo、NexusPHP...」论坛】【百度、谷歌(Google)、必应(Bing)、搜狗、微信、360、Yahoo、Yandex 等搜索引擎...】、贴吧、豆瓣、知乎、NGA、V2EX、煎蛋网、龙的天空、起点中文、千图网、千库网、Pixabay、Pixiv、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、RuTracker、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、小众软件、【动漫狂、动漫屋、漫画猫、漫画屋、漫画 DB、HiComic、Mangabz、Xmanhua 等漫画网站...】、PubMed、Z-Library、GreasyFork、Github、StackOverflow（以上仅一小部分，更多的写不下了...
 // @description:zh-TW  ⭐無縫加載 下一頁內容 至網頁底部（類似瀑布流）⭐，支持各論壇、社交、遊戲、漫畫、小說、學術、搜索引擎(Google、Bing、Yahoo...) 等網站~
@@ -813,11 +813,11 @@ function: {
                 }
             }, //       phpBB 论坛 - 搜索页
             xenforo: {
-                url: ()=> {if (indexOF(/\/(forums|f)\//)) {
+                url: ()=> {if (indexOF(/\/(forums|f)\//) || (getCSS('a.pageNav-jump--next') && getCSS('.structItemContainer-group.js-threadList > div'))) {
                     curSite = DBSite.xenforo;
-                } else if (indexOF(/\/(threads|t)\//)) {
+                } else if (indexOF(/\/(threads|t)\//) || (getCSS('a.pageNav-jump--next') && getCSS('.block-body.js-replyNewMessageContainer > article'))) {
                     curSite = DBSite.xenforo_post;
-                } else if (indexOF('/search/')) {
+                } else if (indexOF('/search/') || (getCSS('a.pageNav-jump--next') && getCSS('ol.block-body > li'))) {
                     curSite = DBSite.xenforo_search;
                 }},
                 pager: {
