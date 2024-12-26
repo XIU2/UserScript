7b47736f0aa0af4fb337b8ae4230e678540058ac
xiu2 <54703944+XIU2@users.noreply.github.com>
2024-12-26 12:52:04 +0800

    更新 外置翻页规则分流地址

--- Autopage.user.js
+++ Autopage.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   自动无缝翻页
 // @name:zh-TW   自動無縫翻頁
 // @name:en      AutoPager
-// @version      6.6.43
+// @version      6.6.44
 // @author       X.I.U
 // @description  ⭐无缝加载 下一页内容 至网页底部（类似瀑布流，无限滚动，无需手动点击下一页）⭐，目前支持：【所有「Discuz!、Flarum、phpBB、MyBB、Xiuno、XenForo、NexusPHP...」论坛】【百度、谷歌(Google)、必应(Bing)、搜狗、微信、360、Yahoo、Yandex 等搜索引擎...】、贴吧、豆瓣、知乎、NGA、V2EX、起点中文、千图网、千库网、Pixabay、Pixiv、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、RuTracker、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、小众软件、【动漫狂、动漫屋、漫画猫、漫画屋、漫画 DB、HiComic、Mangabz、Xmanhua 等漫画网站...】、PubMed、Z-Library、GreasyFork、Github、StackOverflow（以上仅一小部分常见网站，更多的写不下了...
 // @description:zh-TW  ⭐無縫加載 下一頁內容 至網頁底部（類似瀑布流，无限滚动，無需手働點擊下一頁）⭐，支持各論壇、社交、遊戲、漫畫、小說、學術、搜索引擎(Google、Bing、Yahoo...) 等網站~
@@ -91,7 +91,7 @@
     'use strict';
     const urlArr = [ // 外置翻页规则更新地址分流，以确保更新成功率（记得 connect）
         'https://userscript.h233.eu.org/other/Autopage/rules.json',
-        'https://userscript.xiu2.us.kg/other/Autopage/rules.json',
+        //'https://userscript.xiu2.us.kg/other/Autopage/rules.json',
         'https://bitbucket.org/xiu2/userscript/raw/master/other/Autopage/rules.json',
         'https://raw.kkgithub.com/XIU2/UserScript/master/other/Autopage/rules.json',
         'https://gitdl.cn/https://raw.githubusercontent.com/XIU2/UserScript/master/other/Autopage/rules.json',
@@ -112,7 +112,7 @@
         'https://js.cdn.haah.net/gh/XIU2/UserScript/other/Autopage/rules.json',
     ], urlArr2 = [
         'https://userscript.h233.eu.org/other/Autopage/rules.json',
-        'https://userscript.xiu2.us.kg/other/Autopage/rules.json',
+        //'https://userscript.xiu2.us.kg/other/Autopage/rules.json',
         'https://userscript.xiu2.xyz/other/Autopage/rules.json',
         'https://bitbucket.org/xiu2/userscript/raw/master/other/Autopage/rules.json',
         'https://raw.kkgithub.com/XIU2/UserScript/master/other/Autopage/rules.json',
