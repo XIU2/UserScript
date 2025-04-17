86c97938fde47c72f22175c239f8d5c75a1f7c85
xiu2 <54703944+XIU2@users.noreply.github.com>
2025-04-17 15:57:44 +0800

    适配 知乎文章页改动（快捷回到顶部）

--- Zhihu-Enhanced.user.js
+++ Zhihu-Enhanced.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   知乎增强
 // @name:zh-TW   知乎增強
 // @name:ru      Улучшение Zhihu
-// @version      2.3.12
+// @version      2.3.13
 // @author       X.I.U
 // @description  A more personalized Zhihu experience~
 // @description:zh-CN  屏蔽指定类别（视频、盐选、文章、想法、关注[赞同了XX/关注了XX]等等）、屏蔽用户、屏蔽关键词、默认收起回答、快捷收起回答/评论（左键两侧）、快捷回到顶部（右键两侧）、区分问题文章、移除高亮链接、净化搜索热门、净化标题消息、展开问题描述、显示问题作者、默认高清原图（无水印）、置顶显示时间、完整问题时间、直达问题按钮、默认站外直链...
@@ -1545,8 +1545,8 @@ function blockHotOther() {
             }
 
         } else if (location.hostname === 'zhuanlan.zhihu.com'){ //    文章 //
-            backToTop('article.Post-Main.Post-NormalMain'); //                 快捷返回顶部
-            backToTop('div.Post-Sub.Post-NormalSub'); //                       快捷返回顶部
+            backToTop('.Post-content'); //                                     快捷返回顶部
+            backToTop('.Post-Row-Content'); //                                 快捷返回顶部
             setTimeout(topTime_post, 300); //                                  置顶显示时间
             blockUsers(); //                                                   屏蔽指定用户
 
