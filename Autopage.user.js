087023fa40168535027b947db67d0df083bb37f7
xiu2 <54703944+XIU2@users.noreply.github.com>
2025-10-17 18:32:50 +0800

    更新 外置翻页规则分流地址

--- Autopage.user.js
+++ Autopage.user.js
@@ -3,7 +3,7 @@
 // @name:zh-CN   自动无缝翻页
 // @name:zh-TW   自動無縫翻頁
 // @name:ru      Автостраничник
-// @version      6.6.64
+// @version      6.6.65
 // @author       X.I.U
 // @description  ⭐Append the next page content to the bottom seamlessly (like a waterfall, Unlimited scrolling, no need to manually click on the next page) ⭐, support various forums, social networking, games, comics, novels, academics, search engines (Google, Bing, Yahoo...) and other websites~
 // @description:zh-CN  ⭐无缝加载 下一页内容 至网页底部（类似瀑布流，无限滚动，无需手动点击下一页）⭐，目前支持：【所有「Discuz!、Flarum、phpBB、MyBB、Xiuno、XenForo、NexusPHP...」论坛】【百度、谷歌(Google)、必应(Bing)、搜狗、微信、360、Yahoo、Yandex 等搜索引擎...】、贴吧、豆瓣、知乎、NGA、V2EX、起点中文、千图网、千库网、Pixabay、Pixiv、3DM、游侠网、游民星空、NexusMods、Steam 创意工坊、CS.RIN.RU、RuTracker、BT之家、萌番组、动漫花园、樱花动漫、爱恋动漫、AGE 动漫、Nyaa、SrkBT、RARBG、SubHD、423Down、不死鸟、扩展迷、小众软件、【动漫狂、动漫屋、漫画猫、漫画屋、漫画 DB、HiComic、Mangabz、Xmanhua 等漫画网站...】、PubMed、Z-Library、GreasyFork、Github、StackOverflow（以上仅一小部分常见网站，更多的写不下了...
@@ -121,7 +121,7 @@
         //'https://raw.github.site/XIU2/UserScript/master/other/Autopage/rules.json', // 挂了
         //'https://raw.github.store/XIU2/UserScript/master/other/Autopage/rules.json', // 挂了
         'https://raw.dgithub.xyz/XIU2/UserScript/master/other/Autopage/rules.json',
-        'https://ghproxy.net/https://raw.githubusercontent.com/XIU2/UserScript/master/other/Autopage/rules.json',
+        //'https://ghproxy.net/https://raw.githubusercontent.com/XIU2/UserScript/master/other/Autopage/rules.json', // 挂了
     ],
           loadMoreExclude1 = ['.smzdm.com','stackoverflow.com'],
           loadMoreExclude2 = ['.steampowered.com','.zcool.com.cn'];
